2c1355cf42c403b5e4192ac47f6a2ce3798a8c3c
ronaldyu001 <ronaldyu001@gmail.com>
2026-01-19 11:48:04 -0700

    added function to embed conversation

--- backend/Maia/hood/RAG/store_to_vector_store_indices/store_raw_conversation.py
+++ backend/Maia/hood/RAG/store_to_vector_store_indices/store_raw_conversation.py
@@ -1,4 +1,5 @@
-from backend.Maia.hood.context_engineering.helpers.conversations import load_conversation, 
+from backend.Maia.hood.context_engineering.helpers.conversations import load_conversation
+from backend.Maia.hood.context_engineering.helpers.transcript import create_transcript_with_timestamps, trim_transcript
 from backend.logging.LoggingWrapper import Logger
 from backend.Maia.hood.RAG.LlamaIndex_Wrapper import LlamaIndex
 from llama_index.core import VectorStoreIndex
@@ -9,15 +10,38 @@ def store_raw_conversation(session_id: str, index: VectorStoreIndex, persist_dir
 
     #load conversation based on session id
     try:
-        Logger.info(f"Loading {session_id} conversation.")
-        load_conversation(session_id=session_id)
-
+        Logger.info(f"Loading conversation: {session_id}")
+        turns = load_conversation(session_id=session_id)
+        transcript = create_transcript_with_timestamps(turns=turns)
+        stringified_transcript = trim_transcript(transcript=transcript, stringify_entire_transcript=True)
 
     except Exception as err:
         Logger.error(repr(err))
         raise repr(err)
+    
+
+    #create metadata
+    try:
+        Logger.info("Creating conversation metadata.")
+        metadata = {
+            "session_id": session_id,
+            "project": "conversation"
+        }
+
+    except Exception as err:
+        Logger.error(repr(err))
+
 
     #embed conversation to raw_conversations vector store
-    LlamaIndex.embed(
-        
-    )
+    try:
+        Logger.info("Embedding conversation.")
+        LlamaIndex.embed(
+            text=stringified_transcript,
+            metadata=metadata,
+            index=index,
+            persist_dir=persist_dir
+        )
+        Logger.info("Conversation embedded.")
+
+    except Exception as err:
+        Logger.error(repr(err))
